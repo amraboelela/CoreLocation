2c456a0ec9815a08dd7d2188addecd1a70fc2a41
Amr Aboelela <amraboelela@gmail.com>
2017-01-16 12:02:37 -0800

    Created CLLocation

---
+++ Sources/CLLocation.swift
@@ -0,0 +1,16 @@
+//
+//  CLLocation.swift
+//  CoreLocation
+//
+//  Created by Amr Aboelela on 1/16/17.
+//  Copyright © 2017 Amr Aboelela. All rights reserved.
+//  Licensed under Apache License v2.0
+//
+//  See included LICENSE file for license information
+//
+
+import Foundation
+
+open class CLLocation {
+    
+}
--- Sources/CoreLocation.swift
+++
@@ -1,4 +0,0 @@
-struct CoreLocation {
-
-    var text = "Hello, World!"
-}
